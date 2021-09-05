e42d14f4911987deeb7f0c9af57afcf0b1251ebe
Amit Sharma <45700624+Amitsrma@users.noreply.github.com>
2021-09-05 01:19:19 -0400

    Added workcell setup times data and updated WorkCell

--- WorkCell.py
+++ WorkCell.py
@@ -1,3 +1,12 @@
+from data_store import (
+    MANUAL_WORKCELL_SETUP_TIMES, WORKCELL_SETUP_TIMES,
+    MANUAL_CELLS)
+
+
 class Workcell:
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self, cell_number: int):
+        self.cell_number = cell_number
+        if self.cell_number in MANUAL_CELLS:
+            self.time_to_switch = MANUAL_WORKCELL_SETUP_TIMES.get(cell_number)
+        else:
+            self.time_to_switch = WORKCELL_SETUP_TIMES.get(cell_number)
