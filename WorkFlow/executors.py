3e31bc35eac8b4f9c6067b05751e88aa68d1caf9
Amit Sharma <45700624+Amitsrma@users.noreply.github.com>
2021-09-05 11:54:55 -0400

    Add workflow logics

--- WorkFlow/executors.py
+++ WorkFlow/executors.py
@@ -1,1 +1,19 @@
 from Entities.Part import Part
+from EntityManagers import PalletManager
+
+
+def run_system(duration: int = 1):
+    """
+    Executes the logic of system, saves the state of system as it 
+
+    params:
+        duration (int): This the number of weeks system is supposed to run
+                        continuously
+
+    returns: None
+    """
+    duration_in_seconds = duration * 5 * 16 * 3600  # convert duration to sec
+    while duration_in_seconds >= 0:
+        
+        duration_in_seconds -= 1
+    raise NotImplementedError
