cc23ba59e1defc0e7954872dc366b6aad68bae1a
Amit Sharma <45700624+Amitsrma@users.noreply.github.com>
2021-09-05 18:59:47 -0400

    No duplicate ids for pallets and class diagram

--- Entities/Pallet.py
+++ Entities/Pallet.py
@@ -1,10 +1,22 @@
 from Entities.Part import Part
+from typing import Set
 
 
 class Pallet:
-    def __init__(self):
+    __existing_pallet_ids = set()
+    def __init__(self, id: int):
+        if id in self.__existing_pallet_ids:
+            raise ValueError("Duplicate pallet id, pallet ids must be unique")
+        self.__existing_pallet_ids.add(id)
+        self.id = id
         self.is_free = True
         self.assigned_part = None
     
     def assign_part(self, assigned_part: Part) -> None:
         self.assigned_part = assigned_part
+
+    def get_used_pallet_ids(self) -> Set:
+        return self.__existing_pallet_ids
+
+    def __repr__(self) -> str:
+        return f"Pallet: {self.id}"
--- ReadMe.md
+++ ReadMe.md
@@ -3,3 +3,7 @@
 The program will be simlulating the workflow of a belt-conveyor system that runs on specific set of blocking rules.
 
 Goal is to identify and check bottle-necks and optimize performance.
+
+Following is the class diagram showing interactions amontg the entities.
+
+![alt text](./Diagrams/Class-Diagrams.JPG)
